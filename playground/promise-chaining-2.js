e176ae79542f33337b67c75443efa4f767e38124
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2020-06-05 11:26:30 +0530

    users and tasks seperated in routers

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,23 @@
 require("../src/db/mongoose")
 const Task = require("../src/models/task")
 
-Task.findByIdAndDelete("5ed8a3b83e2952e84f92e58a").then(() => {
-    return Task.countDocuments({ completed: false })
-}).then((tasks) => {
-    console.log(tasks);
+// Task.findByIdAndDelete("5ed8a3b83e2952e84f92e58a").then(() => {
+//     return Task.countDocuments({ completed: false })
+// }).then((tasks) => {
+//     console.log(tasks);
+// }).catch((e) => {
+//     console.log(e);
+// })
+//5ed895a8eb2790e26ab1b34b
+const DeleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+
+}
+
+DeleteTaskAndCount("5ed895a8eb2790e26ab1b34b").then((count) => {
+    console.log("Remaining incomplete docs: ", count);
 }).catch((e) => {
-    console.log(e);
+    console.log("Error: ", e);
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -4,11 +4,23 @@ const User = require("../src/models/user")
 
 //5ed89f5e0ad38fe542f68871
 
-User.findByIdAndUpdate("5ed8a35225c7bfe7cb5da6f4", { age: 1 }).then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
+// User.findByIdAndUpdate("5ed8a35225c7bfe7cb5da6f4", { age: 1 }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount("5ed8a35225c7bfe7cb5da6f4", 98).then((result) => {
     console.log(result);
-}).catch((error) => {
-    console.log(error);
+}).catch((e) => {
+    console.log("Error: ", e);
 })
